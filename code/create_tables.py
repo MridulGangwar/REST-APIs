a199477dd200ac194c67b43e6724c549817a05f9
Mridul Gangwar <singhmridul1@gmail.com>
2020-08-31 22:06:35 -0700

    changed get method to retrive from the db

--- code/create_tables.py
+++ code/create_tables.py
@@ -16,5 +16,8 @@ cursor.execute(create_table)
 create_table = "CREATE TABLE IF NOT EXISTS items (name text PRIMARY KEY, price real)"
 cursor.execute(create_table)
 
+cursor.execute("INSERT INTO items VALUES ('chair',349.99)")
+cursor.execute("INSERT INTO items VALUES ('juice',13.99)")
+
 connection.commit()
 connection.close()
